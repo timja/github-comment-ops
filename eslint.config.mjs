49ec3702e16ee7e7bbe3ae059548bb73dc70fc77
Tim Jacomb <timjacomb1@gmail.com>
2025-02-15 22:30:11 +0000

    Upgrade eslint

---
+++ eslint.config.mjs
@@ -0,0 +1,40 @@
+import globals from "globals";
+import path from "node:path";
+import { fileURLToPath } from "node:url";
+import js from "@eslint/js";
+import { FlatCompat } from "@eslint/eslintrc";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+const compat = new FlatCompat({
+  baseDirectory: __dirname,
+  recommendedConfig: js.configs.recommended,
+  allConfig: js.configs.all,
+});
+
+export default [
+  {
+    ignores: ["**/node_modules/", "coverage/**/*"],
+  },
+  ...compat.extends(
+    "eslint:recommended",
+    "plugin:prettier/recommended",
+    "plugin:unicorn/recommended"
+  ),
+  {
+    languageOptions: {
+      globals: {
+        ...globals.commonjs,
+        ...globals.jest,
+        ...globals.node,
+        Atomics: "readonly",
+        SharedArrayBuffer: "readonly",
+      },
+
+      ecmaVersion: 2022,
+      sourceType: "module",
+    },
+
+    rules: {},
+  },
+];
